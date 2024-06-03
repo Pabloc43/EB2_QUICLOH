5f6d117da70546bd0bd45261c4473ecde597cd97
PabloCamejo <pablo.camejo@cencosud.com>
2024-06-03 17:49:47 -0300

    ameizing!

--- elaparato/src/main/java/com/elaparato/repository/IProductoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.elaparato.repository;
-
-import com.elaparato.model.Producto;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface IProductoRepository extends JpaRepository<Producto, Integer> {
-}
